8e2b2faadcae502f13f3788d4a231f2ff8dae6d2
RezaKhoshzaban <88592718+RezaKhoshzaban@users.noreply.github.com>
2022-05-17 18:55:25 +0200

    Add files via upload

--- Ims_Flutter/lib/views/ItemsList.dart
+++ Ims_Flutter/lib/views/ItemsList.dart
@@ -3,9 +3,9 @@ import './../services/http_services.dart';
 
 class ItemsList extends StatelessWidget {
   // final int bookid;
-  final String bookTitle;
-  final String bookAuthor;
-  final String bookGenre;
+  final List bookTitle;
+  final List bookAuthor;
+  final List bookGenre;
 
   // final String bookRFID;
 
@@ -17,21 +17,56 @@ class ItemsList extends StatelessWidget {
     required this.bookGenre,
     // required this.bookRFID,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Book list")),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Center(
-                  child: Text(
-                      "Title: $bookTitle  -  Author : $bookAuthor - Genre : $bookGenre",
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                      textScaleFactor: 2)),
-            ]));
+        appBar: AppBar(title: const Text("Available Titles")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+              name: bookTitle[0],
+              description: bookAuthor[0],
+              price: bookGenre[0],
+            ),
+          ],
+        ));
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  const ProductBox(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price})
+      : super(key: key);
+  final String name;
+  final String description;
+  final String price;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(2),
+        height: 120,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(name,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(description),
+                          Text("Price: " + price.toString()),
+                        ],
+                      )))
+            ])));
   }
 }
