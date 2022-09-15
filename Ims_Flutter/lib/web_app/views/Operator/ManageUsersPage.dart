9a6cda0db62a05cda1d2765076036f656f9073f0
asgari.pouyan <s289607@studenti.polito.it>
2022-09-15 22:23:39 +0200

    merging issues fixed

--- Ims_Flutter/lib/web_app/views/Operator/ManageUsersPage.dart
+++ Ims_Flutter/lib/web_app/views/Operator/ManageUsersPage.dart
@@ -14,80 +14,78 @@ class manageCustomer extends StatelessWidget {
   Widget build(BuildContext context) {
     double widthScreen = MediaQuery.of(context).size.width;
     double heightScreen = MediaQuery.of(context).size.height;
-    return Scaffold( 
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: CustomAppBar(),
-                //alignment: Alignment.topCenter,
-                width: double.infinity,
-                height: 150,
-              ),
-              const Center(
-                  child: Text(
-                "Please select a service : ",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              )),
-              const SizedBox(height: 10),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AddCustomer()));
-                },
-                child: Center(
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 30),
-                    child: const Center(
-                        child: Text("Add user",
-                            style: TextStyle(
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black))),
-                    height: heightScreen/6,
-                    width: widthScreen*0.8,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.15),
-                      border: Border.all(color: Colors.black87),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: CustomAppBar(),
+            //alignment: Alignment.topCenter,
+            width: double.infinity,
+            height: 150,
+          ),
+          const Center(
+              child: Text(
+            "Please select a service : ",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          )),
+          const SizedBox(height: 10),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const AddCustomer()));
+            },
+            child: Center(
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                child: const Center(
+                    child: Text("Add user",
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
+                height: heightScreen / 6,
+                width: widthScreen * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.15),
+                  border: Border.all(color: Colors.black87),
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RemoveCustomer()));
-                },
-                child: Center(
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 30),
-                    child: const Center(
-                        child: Text("Remove user",
-                            style: TextStyle(
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black))),
-                    height: heightScreen/6,
-                    width: widthScreen*0.8,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.15),
-                      border: Border.all(color: Colors.black87),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RemoveCustomer()));
+            },
+            child: Center(
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                child: const Center(
+                    child: Text("Remove user",
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
+                height: heightScreen / 6,
+                width: widthScreen * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.15),
+                  border: Border.all(color: Colors.black87),
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              InkWell(
-                onTap: () async {
+            ),
+          ),
+          InkWell(
+            onTap: () async {
               //await EasyLoading.showSuccess(TheWebUser.length.toString());
               if (TheWebUser[0]['role'] == 'admins') {
                 await Httpservices.WebListUsers("customers", 'ALL', context);
@@ -95,30 +93,29 @@ class manageCustomer extends StatelessWidget {
                 await Httpservices.WebListUsers(TheWebUser[0]['admin_id'],
                     TheWebUser[0]['branch'], context);
               }
-
-                },
-                child: Center(
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 30),
-                    child: const Center(
-                        child: Text("List all users",
-                            style: TextStyle(
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black))),
-                    height: heightScreen/6,
-                    width: widthScreen*0.8,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.15),
-                      border: Border.all(color: Colors.black87),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
+            },
+            child: Center(
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                child: const Center(
+                    child: Text("List all users",
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
+                height: heightScreen / 6,
+                width: widthScreen * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.15),
+                  border: Border.all(color: Colors.black87),
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
             ),
           ),
-        ]));
+        ],
+      ),
+    );
   }
 }
